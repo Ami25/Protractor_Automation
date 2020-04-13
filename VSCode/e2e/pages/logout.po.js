324fb4b6fc34c3b80bc2165845a6d202eba43181
Ami25 <56436560+Ami25@users.noreply.github.com>
2020-04-13 20:14:34 +0530

    Add files via upload

--- VSCode/e2e/pages/logout.po.js
+++ VSCode/e2e/pages/logout.po.js
@@ -1,13 +1,19 @@
-var login=require('../pages/login.po');
 
 var logoutPage = function () {
     'use strict';
     this.img = $('#dropdown-toggle div img');
+    this.help =$('#help-link');
     this.logOut = $('#sign-out-link');    
     //******************** functions *******************
     this.logOutMethod = function () {
         this.img.click();
         this.logOut.click();
+        return true;
+    };
+
+    this.clickHelp = function () {
+        this.img.click();
+        this.help.click();
         return true;
     };
 };
